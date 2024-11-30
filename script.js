237376d5e15f62343b7401549fc4fff8bf8d871f
Kleber Enrique Chavez Oscullo <99308036+kechavez07@users.noreply.github.com>
2024-11-29 23:27:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY!', 'PRISS ❤️','TE QUIERO'],
+			strings: [ 'FELIZ', 'CUMPLEAÑOS!', 'PRISS ❤️','TE QUIERO'],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
